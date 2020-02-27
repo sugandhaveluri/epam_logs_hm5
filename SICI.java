9812266b34c3d934314cbee4b9d41e7f065c0101
sugandha <sugandha.ammini@gmail.com>
2020-02-27 23:40:26 +0530

    first commit

---
+++ SICI.java
@@ -0,0 +1,20 @@
+package com.prolo.com.prolo;
+
+public class SICI {
+	public float SimpleInterest(float principle,float time,float rate)
+	{
+		float res_simpleInterest;
+		/*Calculate simple interest*/
+		res_simpleInterest=principle*time*rate/100;
+		return res_simpleInterest;
+	}
+	public float CompoundInterest(float principle,float time,float rate)
+	{
+		float res_compoundInterest;
+		/* Calculate compound interest*/
+		res_compoundInterest = (float) (principle * (Math.pow((1 + rate / 100), time)));
+		return res_compoundInterest;
+	}
+	
+
+}
