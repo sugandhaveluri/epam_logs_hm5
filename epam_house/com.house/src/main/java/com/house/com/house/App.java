9638474c0006ffcdd131635cb0b566f58963bec1
sugandha <sugandha.ammini@gmail.com>
2020-02-28 11:47:35 +0530

    Updated Construction cost files

---
+++ epam_house/com.house/src/main/java/com/house/com/house/App.java
@@ -0,0 +1,13 @@
+package com.house.com.house;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
